aa1bb73984ad8790f9540bd0f6a1acb68a4886ab
Wes Collins <wescollins@Wes-MacBook-Air.local>
2026-02-18 08:21:32 -0500

    Make photo proxy tolerate missing trailing slash

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "profiles"
@@ -7,5 +7,5 @@ urlpatterns = [
     path("therapists/", views.profiles_list, name="profile_list"),
     path("therapists/edit/", views.profile_edit, name="profile_edit"),
     path("therapists/<slug:slug>/", views.profile_detail, name="profile_detail"),
-    path("therapists/photo-proxy/", views.photo_proxy, name="photo_proxy"),
+    re_path(r"^therapists/photo-proxy/?$", views.photo_proxy, name="photo_proxy"),
 ]
